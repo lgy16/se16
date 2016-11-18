267e7a1dfcb5c01f4e356dc9fee5219482248262
Kim SangWook <tkddnr7979>
2016-11-18 18:49:31 +0900

    Ranking Update & Get Ranking List

--- bubblepop/src/bubblepop/Ranking.java
+++ bubblepop/src/bubblepop/Ranking.java
@@ -1,6 +1,14 @@
 package bubblepop;
 
 import java.awt.Dimension;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.StringTokenizer;
+
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.plaf.basic.BasicTabbedPaneUI;
@@ -19,6 +27,94 @@ import javax.swing.plaf.basic.BasicTabbedPaneUI;
  *
  * @author LGY
  */
+
+class RankingEntry{
+	public RankingEntry(String level, String name, String score){
+		try{		
+			URL url = new URL("http://www.bublepop.esy.es/Register_Ranking/registerRanking.php");
+            HttpURLConnection http = (HttpURLConnection)url.openConnection();
+            http.setDefaultUseCaches(false);
+            http.setDoInput(true);
+            http.setDoOutput(true);
+            http.setRequestMethod("POST");
+
+            http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("?&level="+level+"&name="+name+"&score="+score);
+            
+            OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "UTF-8");
+            PrintWriter writer = new PrintWriter(outStream);
+            writer.write(buffer.toString());
+            writer.flush();
+
+            InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "UTF-8");
+            BufferedReader reader = new BufferedReader(tmp);
+            StringBuilder builder = new StringBuilder();
+            String str;
+            while ((str = reader.readLine()) != null) {
+                builder.append(str);
+            }
+            http.disconnect();
+		}
+		catch(Exception e){
+			System.out.print("Fail : " + e + "\n");
+		}
+		finally {
+			System.out.print("Finish\n");
+		}
+	}
+}
+
+class ShowRanking{
+	public ShowRanking(String level) {
+		try{		
+			URL url = new URL("http://www.bublepop.esy.es/Show_Ranking/showRanking.php");
+            HttpURLConnection http = (HttpURLConnection)url.openConnection();
+            http.setDefaultUseCaches(false);
+            http.setDoInput(true);
+            http.setDoOutput(true);
+            http.setRequestMethod("POST");
+
+            http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
+            StringBuffer buffer = new StringBuffer();
+            buffer.append("?&level="+level);
+            
+            OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "UTF-8");
+            PrintWriter writer = new PrintWriter(outStream);
+            writer.write(buffer.toString());
+            writer.flush();
+
+            InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "UTF-8");
+            BufferedReader reader = new BufferedReader(tmp);
+            StringBuilder builder = new StringBuilder();
+            String str;
+            while ((str = reader.readLine()) != null) {
+                builder.append(str);
+            }
+            Parsing(level, builder.toString(), "/");
+            http.disconnect();
+		}
+		catch(Exception e){
+			System.out.print("Fail : " + e + "\n");
+		}
+		finally {
+			System.out.print("Finish\n");
+		}
+	}
+	
+	void Parsing(String level, String text, String token){
+		StringTokenizer str = new StringTokenizer(text, token);
+		System.out.println("State : " + str.nextToken());
+		System.out.println("----------------------------");
+		while(str.hasMoreTokens()){
+			System.out.println("Name : " + str.nextToken());
+			System.out.println("Tiem : " + str.nextToken());
+			System.out.println("Score : " + str.nextToken());
+			System.out.println("----------------------------");
+		}
+	}
+}
+
 public class Ranking extends javax.swing.JFrame {
 
     /**
